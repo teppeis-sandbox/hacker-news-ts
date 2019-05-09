fc4136f96423d2ab0be189f0a64d395c907414f6
teppeis <teppeis@gmail.com>
2019-05-09 23:15:14 +0900

    add joi

--- src/joi.ts
+++ src/joi.ts
@@ -9,88 +9,116 @@
  * http://www.olioapps.com/blog/checking-types-against-the-real-world-in-typescript/
  */
 import * as joi from "@hapi/joi";
+// import "./joi-extract-type";
 import fetch from "node-fetch";
 
 /* types and validators */
 
 // Type and validator for IDs. This is just an alias for the `number` type.
-const ID_V = joi.number();
-// TODO
-// export type ID = t.typeOf<typeof ID_V>;
+const ID_V = joi.number().required();
+type ID = joi.SchemaValue<typeof ID_V>;
 
 // Type and validator for properties common to all Hacker News item types
 const ItemCommonV = joi.object().keys({
   by: joi.string().required(), // username
   id: ID_V.required(),
-  time: joi.number().required(), // seconds since Unix epoch
+  time: joi
+    .date()
+    .timestamp("unix")
+    .required(), // seconds since Unix epoch
   dead: joi.boolean(),
   deleted: joi.boolean(),
   kids: joi.array().items(ID_V) // IDs of comments on an item
 });
 
 // Type and validator for properties common to stories, job postings, and polls
-const TopLevelV = joi.object().keys(
-  {
-    score: joi.number().required(),
-    title: joi.string().required()
-  }
-);
+const TopLevelV = joi.object().keys({
+  score: joi.number().required(),
+  title: joi.string().required()
+});
 
-const StoryV =    joi.object().keys({
-      type: joi.valid("story").required(),
-      descendants: joi.number().required(), // number of comments
-      text: joi.string(), // HTML content if story is a text post
-      url: joi.string().uri() // URL of linked article if the story is not text post
-    }).concat(
-    ItemCommonV).concat(
-    TopLevelV);
+const StoryV = joi
+  .object()
+  .keys({
+    type: joi
+      .string()
+      .valid("story")
+      .required(),
+    descendants: joi.number().required(), // number of comments
+    text: joi.string(), // HTML content if story is a text post
+    url: joi.string().uri() // URL of linked article if the story is not text post
+  })
+  .concat(ItemCommonV)
+  .concat(TopLevelV)
+  .required();
+type Story = joi.SchemaValue<typeof StoryV>;
 
-// export type Story = joi.object().keysOf<typeof StoryV>;
+const JobV = joi
+  .object()
+  .keys({
+    type: joi
+      .string()
+      .valid("job")
+      .required(),
+    text: joi.string(), // HTML content if job is a text post
+    url: joi.string() // URL of linked page if the job is not text post
+  })
+  .concat(ItemCommonV)
+  .concat(TopLevelV)
+  .required();
+type Job = joi.SchemaValue<typeof JobV>;
 
-const JobV = 
-    joi.object().keys({
-      type: joi.valid("job").required(),
-      text: joi.string(), // HTML content if job is a text post
-      url: joi.string() // URL of linked page if the job is not text post
-    }).concat(
-    ItemCommonV).concat(
-    TopLevelV);
-// export type Job = joi.object().keysOf<typeof JobV>;
+const PollV = joi
+  .object()
+  .keys({
+    type: joi
+      .string()
+      .valid("poll")
+      .required(),
+    descendants: joi.number().required(), // number of comments
+    parts: joi
+      .array()
+      .items(ID_V)
+      .required()
+  })
+  .concat(ItemCommonV)
+  .concat(TopLevelV)
+  .required();
+type Poll = joi.SchemaValue<typeof PollV>;
 
-const PollV = 
-    joi.object().keys({
-      type: joi.valid("poll").required(),
-      descendants: joi.number().required(), // number of comments
-      parts: joi.array().items(ID_V)
-    }).concat(
-    ItemCommonV).concat(
-    TopLevelV)
-// export type Poll = joi.object().keysOf<typeof PollV>;
+const CommentV = joi
+  .object()
+  .keys({
+    type: joi
+      .string()
+      .valid("comment")
+      .required(),
+    parent: ID_V.required(),
+    text: joi.string().required() // HTML content
+  })
+  .concat(ItemCommonV)
+  .required();
+type Comment = joi.SchemaValue<typeof CommentV>;
 
-const CommentV = 
-    joi.object().keys({
-      type: joi.valid("comment").required(),
-      parent: ID_V.required(),
-      text: joi.string().required() // HTML content
-    }).concat(
-    ItemCommonV);
-// export type Comment = joi.object().keysOf<typeof CommentV>;
-
-const PollOptV = joi.object().keys(
-  {
-    type: joi.valid("pollopt").required(),
+const PollOptV = joi
+  .object()
+  .keys({
+    type: joi
+      .string()
+      .valid("pollopt")
+      .required(),
     poll: ID_V.required(), // ID of poll that includes this option
     score: joi.number().required(),
     text: joi.string().required() // HTML content
-  });
-// export type PollOpt = joi.object().keysOf<typeof PollOptV>;
+  })
+  .required();
+type PollOpt = joi.SchemaValue<typeof PollOptV>;
 
-const ItemV = t.taggedUnion(
-  "type", // the name of the tag property
-  [CommentV, JobV, PollV, PollOptV, StoryV],
-  "Item"
-);
-// type Item = joi.object().keysOf<typeof ItemV>;
+const ItemV = joi
+  .alternatives()
+  .try(CommentV, JobV, PollV, PollOptV, StoryV)
+  .required();
+type Item = joi.SchemaValue<typeof ItemV>;
 
 /* functions to fetch and display stories and other items */
 
@@ -98,18 +126,16 @@ export async function fetchItem(id: ID): Promise<Item> {
   const res = await fetch(
     `https://hacker-news.firebaseio.com/v0/item/${id}.json`
   );
-  const obj = await res.json();
-  return decodeToPromise(ItemV, obj);
+  return ItemV.validate(await res.json());
 }
 
 // If you know the type of the item to be fetched use this function with
 // a validator for that specific type.
-async function fetchItemType<T>(validator: joi.object().keys<T>, id: ID): Promise<T> {
+async function fetchItemType<T>(validator: joi.Schema<T>, id: ID): Promise<T> {
   const res = await fetch(
     `https://hacker-news.firebaseio.com/v0/item/${id}.json`
   );
-  const obj = await res.json();
-  return decodeToPromise(validator, obj);
+  return validator.validate(await res.json());
 }
 
 function getTitle(item: Item): string | undefined {
@@ -150,7 +176,13 @@ export async function fetchTopStories(count: number): Promise<Item[]> {
   const res = await fetch(
     "https://hacker-news.firebaseio.com/v0/topstories.json"
   );
-  const ids = await decodeToPromise(t.array(ID_V), await res.json());
+  const ids = joi.attempt(
+    await res.json(),
+    joi
+      .array()
+      .items(ID_V)
+      .required()
+  );
   return Promise.all(ids.slice(0, count).map(id => fetchItem(id)));
 }
 
@@ -173,37 +205,22 @@ if (require.main === module) {
 
 /* utility functions */
 
-// Apply a validator and get the result in a `Promise`
-function decodeToPromise<T, O, I>(
-  validator: joi.object().keys<T, O, I>,
-  input: I
-): Promise<T> {
-  const result = validator.decode(input);
-  return result.fold(
-    errors => {
-      const messages = reporter(result);
-      return Promise.reject(new Error(messages.join("\n")));
-    },
-    value => Promise.resolve(value)
-  );
-}
+// async function fetchTitle(storyId: number): Promise<string> {
+//   const res = await fetch(
+//     `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
+//   );
+//   const data = await res.json();
 
-async function fetchTitle(storyId: number): Promise<string> {
-  const res = await fetch(
-    `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
-  );
-  const data = await res.json();
+//   // If the data that is fetched does not match the `StoryV` validator then this
+//   // line will result in a rejected promise.
+//   const story = await decodeToPromise(StoryV, data);
 
-  // If the data that is fetched does not match the `StoryV` validator then this
-  // line will result in a rejected promise.
-  const story = await decodeToPromise(StoryV, data);
+//   // This line does not type-check because TypeScript can infer from the
+//   // definition of `StoryV` that `story` does not have a property called
+//   // `descendents`.
+//   // const ds = story.descendents;
 
-  // This line does not type-check because TypeScript can infer from the
-  // definition of `StoryV` that `story` does not have a property called
-  // `descendents`.
-  // const ds = story.descendents;
-
-  // TypeScript infers that `story` does have a `title` property with a value of
-  // type `string`, so this passes type-checking.
-  return story.title;
-}
+//   // TypeScript infers that `story` does have a `title` property with a value of
+//   // type `string`, so this passes type-checking.
+//   return story.title;
+// }
